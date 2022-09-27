fc5af554b68061472cb69a3eeba0a3e24d6b39a6
Agnieszka Wenerska <wenerskaaga@gmail.com>
2022-09-27 14:20:37 +0200

    Remove class active from articles

--- js/script.js
+++ js/script.js
@@ -3,10 +3,20 @@ const titleClickHandler = function(event){
     console.log('MouseEvent', event);
   
     /* remove class 'active' from all article links  */
+const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
   
     /* add class 'active' to the clicked link */
   
     /* remove class 'active' from all articles */
+const activeArticles = document.querySelectorAll('.post');
+
+for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+}
   
     /* get 'href' attribute from the clicked link */
   
