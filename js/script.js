428d7710dd6eef4caee0567183b28b406d84d30e
Agnieszka Wenerska <wenerskaaga@gmail.com>
2022-09-28 00:10:48 +0200

    Add class active to article

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log('MouseEvent', event);
+    
   
     /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
@@ -10,24 +12,29 @@ const titleClickHandler = function(event){
     activeLink.classList.remove('active');
     }
   
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
     clickedElement.classList.add('active');
     console.log('clickedElement:', clickedElement);    
     
     /* [DONE] remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll('.post');
+    const activeArticles = document.querySelectorAll('article');
 
     for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
+    }
+  
+    /* [DONE] get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
+  
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
+  
+    /* [DONE] add class 'active' to the correct article */
+    targetArticle.classList.add('active');
+    console.log('clickedElement:', clickedElement);    
 }
-  
-    /* get 'href' attribute from the clicked link */
-  
-    /* find the correct article using the selector (value of 'href' attribute) */
-  
-    /* add class 'active' to the correct article */
-  }
-  
   const links = document.querySelectorAll('.titles a');
   
   for(let link of links){
