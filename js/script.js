06c25492a7aa1714ecc5936c6d87e01870f8e5ca
Agnieszka Wenerska <wenerskaaga@gmail.com>
2022-09-27 17:01:05 +0200

    Add active class for links

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
 const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
     console.log('MouseEvent', event);
   
-    /* remove class 'active' from all article links  */
-const activeLinks = document.querySelectorAll('.titles a.active');
+    /* [DONE] remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
-for(let activeLink of activeLinks){
-  activeLink.classList.remove('active');
-}
+    for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+    }
   
-    /* add class 'active' to the clicked link */
-  
-    /* remove class 'active' from all articles */
-const activeArticles = document.querySelectorAll('.post');
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
+    console.log('clickedElement:', clickedElement);    
+    
+    /* [DONE] remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.post');
 
-for(let activeArticle of activeArticles){
+    for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
 }
   
